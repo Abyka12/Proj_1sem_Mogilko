bb08337d7ac88c74f45fea569a62384b0619a1b6
Abyka12 <antondvachevsky@yandex.ru>
2022-12-10 12:53:24 +0300

    PZ #9

--- PZ_9/PZ_9_1.py
+++ PZ_9/PZ_9_1.py
@@ -1,7 +1,22 @@
+# Вариант 29
+# Выполнить сортировку словаря d = {'a': 5, 'b': 30, 'c': 3}
+
+
 d = {'a': 5, 'b': 30, 'c': 3}
+items = d.items
+print(f"Дан словарь {d}, его нужно отсортировать")
+a = input("Выберите тип сортировки: \n"
+          "1 - ключи \n"
+          "2 - значения\n"
+          "Введите знаечние: ")
 
+if a == '1':
+    sorted_items = sorted(items(), key=lambda x: x[0])
+    print(dict(sorted_items))
 
+elif a == '2':
+    sorted_items = sorted(items(), key=lambda x: x[1])
+    print(dict(sorted_items))
 
-
-
-# print(sorted_d)
+else:
+    print("Введено неверное значение")
